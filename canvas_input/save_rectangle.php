60b88decb17b7c75c972a6abd591c6508e6bcded
Wanting Mao <97856917+wantingmao01@users.noreply.github.com>
2024-02-25 00:12:20 -0800

    Delete canvas_input directory

--- canvas_input/save_rectangle.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = file_get_contents('php://input');
-    file_put_contents('request_body.json', $data);
-    echo 'Request body written to file.';
-}
-?>
