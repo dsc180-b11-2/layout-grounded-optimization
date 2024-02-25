f11ca32efd5d899f3281f71c17a300a79fe1020a
Wanting Mao <97856917+wantingmao01@users.noreply.github.com>
2024-02-25 00:13:12 -0800

    Delete utils directory

--- utils/api_key.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# You can either set `OPENAI_API_KEY` environment variable or replace "YOUR_API_KEY" below with your OpenAI API key
-if "OPENAI_API_KEY" in os.environ:
-    api_key = os.environ["OPENAI_API_KEY"]
-else:
-    api_key = "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"
